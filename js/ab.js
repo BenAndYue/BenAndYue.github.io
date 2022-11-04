99b1fc6cdc855bc22ca646ff07b1ded7e17ddf1e
BenAndYue <92823586+BenAndYue@users.noreply.github.com>
2022-11-04 19:12:05 +0100

    cleanup

---
+++ js/ab.js
@@ -0,0 +1,47 @@
+
+
+function goed() {
+    document.getElementById('goedInput').setAttribute('visible', 'true')
+
+}
+
+function fout() {
+    document.getElementById('foutInput').setAttribute('visible', 'true')
+
+}
+
+// scene switch
+
+//switching between scenes.
+function setScene1() {
+    document.getElementById('scene1').setAttribute('visible', 'true')
+    document.getElementById('sceneFarm').setAttribute('visible', 'false')
+    document.getElementById('sceneStore').setAttribute('visible', 'false')
+    document.getElementById('sceneMeeting').setAttribute('visible', 'false')
+    document.getElementById('sceneHome').setAttribute('visible', 'false')
+    document.getElementById('foutInput').setAttribute('visible', 'false')
+    document.getElementById('goedInput').setAttribute('visible', 'false')
+
+
+}
+
+function setSceneFarm() {
+    document.getElementById('scene1').setAttribute('visible', 'false')
+    document.getElementById('sceneFarm').setAttribute('visible', 'true')
+}
+
+function setSceneStore() {
+    document.getElementById('scene1').setAttribute('visible', 'false')
+    document.getElementById('sceneStore').setAttribute('visible', 'true')
+}
+
+function setSceneMeeting() {
+    document.getElementById('scene1').setAttribute('visible', 'false')
+    document.getElementById('sceneMeeting').setAttribute('visible', 'true')
+}
+
+function setSceneHome() {
+    document.getElementById('scene1').setAttribute('visible', 'false')
+    document.getElementById('sceneHome').setAttribute('visible', 'true')
+}
+
