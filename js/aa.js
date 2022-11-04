6413d7d9c1d80575a7122fa56531cebb8bbde604
BenAndYue <92823586+BenAndYue@users.noreply.github.com>
2022-11-04 02:42:22 +0100

    edit

--- js/aa.js
+++ js/aa.js
@@ -2,6 +2,7 @@
 
 let j = 0;
 let box, projectile, z = 0, dz = -0.1;
+let points = 0;
 
 
 window.onload = function () {
@@ -13,7 +14,13 @@ window.onload = function () {
     target = document.getElementById("target")
     ball = document.getElementById("ballp")
 
-    //voor + op de score
+    //targets
+    target100 = document.getElementById("target100")
+    target200 = document.getElementById("target200")
+    target300 = document.getElementById("target300")
+    target500 = document.getElementById("target500")
+    target1001 = document.getElementById("target1001")
+    target1002 = document.getElementById("target1002")
 
     loop()
 
@@ -22,21 +29,73 @@ window.onload = function () {
 function loop() {
     setTimeout(loop, 50)
 
-   //500 punten
-   if(collision(target, ball)){
-       console.log("DONE")
-       ball.remove()
+    //100 punten
+    if (collision(target100, ball)) {
+        // console.log("DONE")
+        // ball.remove()
+        score.setAttribute("value", points + 100 + " punten")
+    }
 
-       j = 500
-       score.setAttribute("value", j + " punten" )
+    //200 punten
+    if (collision2(target200, ball)) {
+        console.log("DONE")
+        ball.remove()
+        score.setAttribute("value", points + 200 + " punten")
+    }
+    //300
+    if (collision2(target300, ball)) {
+        console.log("DONE")
+        ball.remove()
+        score.setAttribute("value", points + 300 + " punten")
+    }
+    //500
+    if (collision2(target500, ball)) {
+        console.log("DONE")
+        ball.remove()
+        score.setAttribute("value", points + 500 + " punten")
+    }
+//    1000
+    if (collision2(target1001, ball)) {
+        console.log("DONE")
+        ball.remove()
+        score.setAttribute("value", points + 1000 + " punten")
+    }
+    if (collision2(target1002, ball)) {
+        console.log("DONE")
+        ball.remove()
+        score.setAttribute("value", points + 1000 + " punten")
+    }
 
-   }
+
 }
 
 
 //method to check if target hits x
 //obj1 = target, 2 = ball
 function collision(obj1, obj2) {
+
+    let x1 = obj1.object3D.position.x;
+    let z1 = obj1.object3D.position.z;
+    let y1 = obj1.object3D.position.y;
+
+    let x2 = obj2.object3D.position.x;
+    let z2 = obj2.object3D.position.z;
+    let y2 = obj2.object3D.position.y;
+
+
+    const distance = Math.sqrt(
+        (x1 - x2) * (x1 - x2) +
+        (y1 - y2) * (y1 - y2) +
+        (z1 - z2) * (z1 - z2)
+    );
+
+    //radius
+    // console.log(distance)\
+
+    return distance < 1.1;
+}
+
+function collision2(obj1, obj2) {
 
 
     let x1 = obj1.object3D.position.x;
@@ -56,11 +115,8 @@ function collision(obj1, obj2) {
 
     //radius
     // console.log(distance)\
-console.log(distance)
-    return distance < 0.95;
 
-
-
+    return distance < 0.7;
 }
 
 
