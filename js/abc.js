2e56f3f8f8e33262ff8464d7075edf8dd535eca7
BenAndYue <92823586+BenAndYue@users.noreply.github.com>
2022-11-05 01:48:41 +0100

    added 360 video playback

--- js/abc.js
+++ js/abc.js
@@ -41,14 +41,63 @@ AFRAME.registerComponent('sphereexpand', {
             introelements.forEach((introelement) => {
                 introelement.setAttribute('visible', true)
             })
-            // document.getElementById('intro').setAttribute('visible', 'true')
 
         }
 
+        this.el.addEventListener('click', sphereloader);
+    }
+});
+
+AFRAME.registerComponent('tostart', {
+
+    init: function () {
+
+        let current = document.querySelectorAll(".intro");
+        let next = document.querySelectorAll(".start");
+
+
+        let sphereloader = () => {
+            sky.setAttribute("src", "#bordeauxtheater");
+            current.forEach((currentelement) => {
+                currentelement.setAttribute("visible", false)
+            })
+            loadingInintro()
+
+        }
+        let loadingInintro = () => {
+            next.forEach((nextelement) => {
+                nextelement.setAttribute('visible', true)
+            })
+
+        }
 
         this.el.addEventListener('click', sphereloader);
+    }
+});
 
+AFRAME.registerComponent('to1', {
 
+    init: function () {
+
+        let current = document.querySelectorAll(".start");
+        let next = document.querySelectorAll(".scene1");
+        let sphereloader = () => {
+            console.log("start")
+            sky.setAttribute("src", "#test360");
+
+            current.forEach((currentelement) => {
+                currentelement.setAttribute("visible", false)
+            })
+            loadingInintro()
+        }
+        let loadingInintro = () => {
+            next.forEach((nextelement) => {
+                nextelement.setAttribute('visible', true)
+            })
+
+        }
+
+        this.el.addEventListener('click', sphereloader);
     }
 });
 
