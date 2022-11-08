0a675a1be9c5dd69c170e0f49e1bca05c6c3281d
BenAndYue <92823586+BenAndYue@users.noreply.github.com>
2022-11-08 19:49:00 +0100

    fix

--- js/scene1.js
+++ js/scene1.js
@@ -1,75 +1,75 @@
-//sla op in cookies
-//half erbij na goed zijn.
-//TODO check if cookies is already used otherwise add points.
-//TODO go back methode naar scene 1 to1
-// Vraag naar scene
-AFRAME.registerComponent('1a', {
-    init: function () {
-        let current = document.querySelectorAll(".scene11");
-        let next = document.querySelectorAll(".scene1a");
-        let sphereloader = () => {
-            //set vraag 1a
-            sky.setAttribute("src", "#scen1a");
-            current.forEach((currentelement) => {
-                currentelement.setAttribute("visible", false)
-            })
-            loadingInintro()
-        }
-        let loadingInintro = () => {
-            next.forEach((nextelement) => {
-                nextelement.setAttribute('visible', true)
-            })
-        }
-        this.el.addEventListener('click', sphereloader);
-    }
-});
-
-AFRAME.registerComponent('1b', {
-    init: function () {
-        let current = document.querySelectorAll(".scene11");
-        let next = document.querySelectorAll(".scene1b");
-        let sphereloader = () => {
-            //set vraag 1a
-            sky.setAttribute("src", "#scene1b");
-            current.forEach((currentelement) => {
-                currentelement.setAttribute("visible", false)
-            })
-            loadingInintro()
-        }
-        let loadingInintro = () => {
-            next.forEach((nextelement) => {
-                nextelement.setAttribute('visible', true)
-            })
-        }
-        this.el.addEventListener('click', sphereloader);
-    }
-});
-
-AFRAME.registerComponent('1c', {
-    init: function () {
-        let current = document.querySelectorAll(".scene11");
-        let next = document.querySelectorAll(".scene1c");
-        let sphereloader = () => {
-            //set vraag 1a
-            // sky.setAttribute("src", "#bordeauxtheater");
-            current.forEach((currentelement) => {
-                // currentelement.setAttribute("visible", false);
-                var secondCameraEl = document.getElementById("second-camera");
-                console.log(secondCameraEl)
-                secondCameraEl.setAttribute('camera', 'active', true);
-            })
-
-
-            // loadingInintro()
-        }
-        let loadingInintro = () => {
-            next.forEach((nextelement) => {
-                nextelement.setAttribute('visible', true)
-            })
-        }
-        this.el.addEventListener('click', sphereloader);
-    }
-});
-//TODO scene naar board
-
-//TODO board naar scene 2
+// //sla op in cookies
+// //half erbij na goed zijn.
+// //TODO check if cookies is already used otherwise add points.
+// //TODO go back methode naar scene 1 to1
+// // Vraag naar scene
+// AFRAME.registerComponent('1a', {
+//     init: function () {
+//         let current = document.querySelectorAll(".scene11");
+//         let next = document.querySelectorAll(".scene1a");
+//         let sphereloader = () => {
+//             //set vraag 1a
+//             sky.setAttribute("src", "#scen1a");
+//             current.forEach((currentelement) => {
+//                 currentelement.setAttribute("visible", false)
+//             })
+//             loadingInintro()
+//         }
+//         let loadingInintro = () => {
+//             next.forEach((nextelement) => {
+//                 nextelement.setAttribute('visible', true)
+//             })
+//         }
+//         this.el.addEventListener('click', sphereloader);
+//     }
+// });
+//
+// AFRAME.registerComponent('1b', {
+//     init: function () {
+//         let current = document.querySelectorAll(".scene11");
+//         let next = document.querySelectorAll(".scene1b");
+//         let sphereloader = () => {
+//             //set vraag 1a
+//             sky.setAttribute("src", "#scene1b");
+//             current.forEach((currentelement) => {
+//                 currentelement.setAttribute("visible", false)
+//             })
+//             loadingInintro()
+//         }
+//         let loadingInintro = () => {
+//             next.forEach((nextelement) => {
+//                 nextelement.setAttribute('visible', true)
+//             })
+//         }
+//         this.el.addEventListener('click', sphereloader);
+//     }
+// });
+//
+// AFRAME.registerComponent('1c', {
+//     init: function () {
+//         let current = document.querySelectorAll(".scene11");
+//         let next = document.querySelectorAll(".scene1c");
+//         let sphereloader = () => {
+//             //set vraag 1a
+//             // sky.setAttribute("src", "#bordeauxtheater");
+//             current.forEach((currentelement) => {
+//                 // currentelement.setAttribute("visible", false);
+//                 var secondCameraEl = document.getElementById("second-camera");
+//                 console.log(secondCameraEl)
+//                 secondCameraEl.setAttribute('camera', 'active', true);
+//             })
+//
+//
+//             // loadingInintro()
+//         }
+//         let loadingInintro = () => {
+//             next.forEach((nextelement) => {
+//                 nextelement.setAttribute('visible', true)
+//             })
+//         }
+//         this.el.addEventListener('click', sphereloader);
+//     }
+// });
+// //TODO scene naar board
+//
+// //TODO board naar scene 2
